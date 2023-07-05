ef944ec1c50cf305d2c9b2c8a5912257dc2f4308
Cksuraj <sjchaudhary00@gmail.com>
2023-07-05 20:17:45 +0530

    Webdev-css-2

--- Assignment-8-Recurrsion/compress.java
+++ Assignment-8-Recurrsion/compress.java
@@ -1,30 +1,28 @@
 import java.util.*;
 
 public class compress {
- public static int comp(char[] chars) {
-
+    public static int compre(char[] chars) {
         char t = ' ';
         int ans = 0, counter;
         for (int i = 0; i < chars.length;) {
-
             t = chars[i];
             counter = 0;
-
             while (i < chars.length && chars[i] == t) {
                 counter++;
                 i++;
             }
-
             chars[ans++] = t;
-            if (counter > 1)
-                for (final char c : String.valueOf(counter).toCharArray())
+            if (counter > 1) {
+                for (char c : String.valueOf(counter).toCharArray()) {
                     chars[ans++] = c;
+                }
+            }
         }
-
         return ans;
     }
+
     public static void main(String[] args) {
-   String  chars[] = {"a","a","b","b","c","c","c"}; 
-   System.out.println(compress.comp(chars));       
+        char[] chars = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+        System.out.println(compre(chars));
     }
 }
